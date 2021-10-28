b88ac2aa96809452d4095db9790aaa3e062c5d81
kjcarter-design <k.carter.design@icloud.com>
2021-10-27 21:16:31 -0400

    initial commit

--- src/nwd/java/Main.java
+++ src/nwd/java/Main.java
@@ -3,6 +3,9 @@ package nwd.java;
 public class Main {
 
     public static void main(String[] args) {
+        /*find more info on Primitive Data Types at
+https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html*/
+
 
     }
 }
